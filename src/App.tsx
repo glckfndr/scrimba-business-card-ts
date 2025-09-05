51e1ad5171daaac0c201b590c079340e9b26bf10
glckfndr <obulan65@gmail.com>
2025-09-05 15:42:59 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Info from "./components/Info";
+import Footer from "./components/Footer";
+import Interests from "./components/Interests";
+import About from "./components/Interests";
+
+function App() {
+  return (
+    <>
+      <Info />
+      <About />
+      <Interests />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Info.tsx
@@ -0,0 +1,8 @@
+export default function Info() {
+  return (
+    <div>
+      <h1>Info Page</h1>
+      <p>This is the info page of the application.</p>
+    </div>
+  );
+}
