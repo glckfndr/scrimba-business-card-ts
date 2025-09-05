e39df9d74fcc2a1cd52585728a49673253e27e0d
glckfndr <obulan65@gmail.com>
2025-09-05 16:44:13 +0300

    feat: add Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
+import "./App.css";
 import Info from "./components/Info";
 import Footer from "./components/Footer";
 import Interests from "./components/Interests";
 import About from "./components/Interests";
+import Card from "./components/Card";
 
 function App() {
   return (
-    <>
-      <Info />
-      <About />
-      <Interests />
-      <Footer />
-    </>
+    <div className="App">
+      <Card>
+        <Info />
+        <About />
+        <Interests />
+        <Footer />
+      </Card>
+    </div>
   );
 }
 
--- src/components/Interests.tsx
+++ src/components/Interests.tsx
@@ -1,8 +1,8 @@
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
-      <p>This is the about page of the application.</p>
+      <h1>Interests Page</h1>
+      <p>This is the Interests page of the application.</p>
     </div>
   );
 }
